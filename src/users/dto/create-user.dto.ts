5d3f6d3fdd5c03812ec774013bc9d131b10a172c
MIHMahmudEli <mdmohsin11222@gmail.com>
2026-05-08 12:25:23 +0600

    refactor: update minimum password length to 8 in CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -12,7 +12,7 @@ export class CreateUserDto {
 
   @IsString()
   @IsNotEmpty({ message: 'Password is required' })
-  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
 
   @IsEnum(UserRole, { message: 'Invalid user role' })
