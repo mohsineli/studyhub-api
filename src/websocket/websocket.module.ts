d93a32f304279ff71dbaac3ddfc73ac3ae7c57ae
MIHMahmudEli <mdmohsin11222@gmail.com>
2026-06-08 14:06:21 +0600

    fix: import UsersModule in WebsocketModule to resolve ActivityService dependency

--- src/websocket/websocket.module.ts
+++ src/websocket/websocket.module.ts
@@ -2,10 +2,11 @@ import { Module, Global } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { WebsocketGateway } from './websocket.gateway';
 import { WebsocketService } from './websocket.service';
+import { UsersModule } from '../users/users.module';
 
 @Global()
 @Module({
-  imports: [JwtModule],
+  imports: [JwtModule, UsersModule],
   providers: [WebsocketGateway, WebsocketService],
   exports: [WebsocketService],
 })
